aef05b378305cd0d007a7f3762fea9955bb16893
xuchenghua <xuchenghua@loongson.cn>
2015-10-20 11:15:37 +0800

    binutils-2.24: Customizing Loongson 3A to mips64r2.
        Change the options --march=loonson3a for mips64r2 insn set.
    
    Change-Id: I6795ac1c344d793054eb2836f552e15843d75658

--- gas/config/tc-mips.c
+++ gas/config/tc-mips.c
@@ -17779,9 +17779,10 @@ static const struct mips_cpu_info mips_cpu_info_table[] =
   /* Broadcom SB-1A CPU core */
   { "sb1a",           0, ASE_MIPS3D | ASE_MDMX,	ISA_MIPS64,   CPU_SB1 },
   
-  { "loongson3a",     0, 0,			ISA_MIPS64,   CPU_LOONGSON_3A },
 
   /* MIPS 64 Release 2 */
+  
+  { "loongson3a",     0, 0,			ISA_MIPS64R2,   CPU_LOONGSON_3A },
 
   /* Cavium Networks Octeon CPU core */
   { "octeon",	      0, 0,			ISA_MIPS64R2, CPU_OCTEON },
